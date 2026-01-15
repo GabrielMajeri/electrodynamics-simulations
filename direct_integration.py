a633c722a3e0f742ac5ed2472356e0d061cece65
Gabriel Majeri <gabriel.majeri6@gmail.com>
2026-01-15 17:52:16 +0200

    Reduce trajectory radius to prevent electrons from going faster than the speed of light

--- direct_integration.py
+++ direct_integration.py
@@ -23,7 +23,11 @@ large_circle_radius = 50 * lmbd
 
 # How many timestamps to use (will affect trajectory sampling frequency)
 num_timestamps = 1024
-integration_time = 10 * (2 * np.pi) / omega_laser
+
+# Determine for how long the simulation/numerical integration will run
+# We'll use an integer multiple of the laser pulse's period
+num_periods = 10
+integration_time = num_periods * (2 * np.pi) / omega_laser
 timestamps = np.linspace(0, integration_time, num_timestamps)
 
 particle_indices = np.arange(num_particles, dtype=np.int32)
@@ -45,7 +49,13 @@ cutoff = np.exp(-1 / (timestamps / slope + 1e-10) ** 2) * np.exp(
     -1 / (((integration_time - timestamps) / slope + 1e-10) ** 2)
 )
 
-trajectory_radius = 5 * lmbd * cutoff
+trajectory_radius = 0.75 * lmbd * cutoff
+
+# Compute instantaneous velocities and make sure we don't go faster than the speed of light
+# (this might happen if we set a trajectory with too large a circumference)
+assert np.all(trajectory_radius / (2 * np.pi / omega_laser) < c), (
+    "Particles move faster than speed of light"
+)
 
 trajectories[:, :, 0] = (
     centers[:, 0, np.newaxis]
@@ -98,7 +108,7 @@ ax.scatter(*detector_position, s=10)
 ax.set_xlabel("$x$")
 ax.set_ylabel("$y$")
 ax.set_zlabel("$z$")
-plt.savefig("setup.pdf")
+plt.savefig("experimental_setup.pdf")
 # plt.show()
 plt.close()
 
@@ -111,7 +121,7 @@ plt.plot(timestamps, particle_displacements[0, :, 0], label="x")
 plt.plot(timestamps, particle_displacements[0, :, 1], label="y")
 plt.legend()
 plt.grid()
-plt.savefig("r_0.pdf")
+plt.savefig("particle_trajectory.pdf")
 plt.close()
 
 # Compute detector displacement (in each particle frame of reference)
@@ -131,11 +141,35 @@ frequency = omega_laser * 1.00
 n_0_dot_r_0 = np.vecdot(n_0s[0], r_0s[0])
 exponent = frequency * timestamps - frequency / c * n_0_dot_r_0
 
-# Plot (real-valued) exponent of oscillatory kernel
+plt.title("$\\frac{d}{dt} \\left(n_0 \\cdot r_0\\right) (t)$")
+
+plt.plot(timestamps, np.gradient(n_0_dot_r_0), label="Derivative")
+plt.axhline(c, xmin=0, xmax=integration_time, color="orange", label="Speed of light")
+
+plt.grid()
+plt.legend()
+
+plt.xlabel("$t$")
+plt.ylabel("Derivative")
+
+plt.savefig("derivative_of_n_0_dot_r_0.pdf")
+plt.close()
+
+# Check again that the exponent doesn't go to 0
+assert np.all(np.abs(np.gradient(n_0_dot_r_0, axis=-1)) < c)
+
+# Plot imaginary part of the exponent of the oscillatory kernel
 plt.title("Exponent ($g(t)$)")
 plt.plot(timestamps, exponent)
 plt.grid()
 plt.savefig("g.pdf")
+plt.close()
+
+# Plot the derivative of the exponent
+plt.title("$g'(t)$")
+plt.plot(timestamps, np.gradient(exponent))
+plt.grid()
+plt.savefig("dg_dt.pdf")
 plt.close()
 
 oscillatory_kernel = np.exp(1j * exponent)
