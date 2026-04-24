Fix units and reproduce plots from paper

laguerre_gauss_beam_momentum_transfer.py
@@ -2,10 +2,12 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 from electrodynamics.beams import (
+    PolarizationVector,
     compute_electric_and_magnetic_field_for_gaussian_beam,
     compute_electric_and_magnetic_field_for_laguerre_gauss_beam,
     compute_electric_and_magnetic_field_for_plane_wave,
 )
+from electrodynamics.constants import c, lmbd, omega_laser
 from electrodynamics.initial_conditions import generate_initial_positions_on_disk
 from electrodynamics.iterate import iterate_initial_conditions_hamiltonian_system
 from electrodynamics.plotting import (
@@ -28,7 +30,7 @@ def main() -> int:
     #     - Momentum distribution at the end (i.e. histogram of the velocities)
 
     rng = np.random.default_rng(seed=17)
-    radius = 4
+    radius = 250 * lmbd
     num_particles = 16384
     initial_positions = generate_initial_positions_on_disk(rng, radius, num_particles)
 
@@ -47,7 +49,7 @@ def main() -> int:
     initial_velocities[:, 0] = 1
 
     # TODO: pick some realistic values
-    particle_charge = 1
+    particle_charge = -1
     particle_mass = 1
 
     azimuthal_index = -2
@@ -59,24 +61,63 @@ def main() -> int:
     # beam_type = "gaussian"
     beam_type = "laguerre_gauss"
 
+    def cutoff(phi: RealArray, phi_0: float, tau_0: float) -> RealArray:
+        return np.exp(-(((phi - phi_0) / tau_0) ** 2))
+
+    tau_0 = 10 / omega_laser
+    # tau_0 = 100 / omega_laser
+    print("Tau 0 =", tau_0)
+    phi_0 = 3 * tau_0
+    integration_time = 6 * tau_0
+    print("Integration time:", integration_time)
+
+    phi = np.linspace(0, integration_time, 128, dtype=np.float64)
+
+    plt.figure()
+    plt.plot(phi, cutoff(phi, phi_0=phi_0, tau_0=tau_0))
+    plt.savefig("plots/cutoff.pdf")
+    plt.close()
+
+    a_0 = 1e-2
+    amplitude = a_0 * c * omega_laser
+    # polarization = PolarizationVector(1, 0)
+    polarization = PolarizationVector(1 / np.sqrt(2), 1j / np.sqrt(2))
+    wavelength = lmbd
+    waist_radius = 75 * wavelength
+
     def acceleration(q: RealArray, p: RealArray) -> RealArray:
         laboratory_time = q[:, 0]
         position_vectors = q[:, 1:4]
 
         if beam_type == "plane_wave":
             E, B = compute_electric_and_magnetic_field_for_plane_wave(
-                position_vectors, laboratory_time
+                positions=position_vectors,
+                time=laboratory_time,
             )
         elif beam_type == "gaussian":
             E, B = compute_electric_and_magnetic_field_for_gaussian_beam(
-                position_vectors, laboratory_time
+                polarization=polarization,
+                positions=position_vectors,
+                time=laboratory_time,
             )
         elif beam_type == "laguerre_gauss":
             E, B = compute_electric_and_magnetic_field_for_laguerre_gauss_beam(
-                position_vectors, laboratory_time, l=azimuthal_index, p=radial_index
+                amplitude=amplitude,
+                waist_radius=waist_radius,
+                wavelength=wavelength,
+                radial_index=radial_index,
+                azimuthal_index=azimuthal_index,
+                polarization=polarization,
+                positions=position_vectors,
+                time=laboratory_time,
             )
         else:
             raise NotImplementedError(f"unsupported beam type '{beam_type}'")
+
+        cut = cutoff(laboratory_time - position_vectors[:, 2] / c, phi_0, tau_0)
+        cut = cut.reshape(-1, 1)
+        E *= cut
+        B *= cut
 
         # print(E, B)
 
@@ -98,7 +139,11 @@ def main() -> int:
         return acceleration
 
     times, positions, velocities = iterate_initial_conditions_hamiltonian_system(
-        acceleration, initial_positions, initial_velocities, integration_time=10
+        acceleration,
+        initial_positions,
+        initial_velocities,
+        integration_time=integration_time,
+        time_step=5,
     )
 
     final_positions = positions[-1, :, 1:4]
@@ -112,7 +157,7 @@ def main() -> int:
     fig, axes = plt.subplots(1, 2, figsize=(10, 6))
 
     for index in range(1):
-        axes[0].plot(times, positions[:, index, 0], label="$t$")
+        # axes[0].plot(times, positions[:, index, 0], label="$t$")
         axes[0].plot(times, positions[:, index, 1], label="$x$")
         axes[0].plot(times, positions[:, index, 2], label="$y$")
         axes[0].plot(times, positions[:, index, 3], label="$z$")
@@ -132,8 +177,10 @@ def main() -> int:
 
     fig.savefig("plots/electron_trajectories.pdf")
 
+    print("Maximum angular momentum:", angular_momentum_along_z_axis.max())
+
     plot_angular_momentum_distribution(
-        initial_positions[:, 1:4], angular_momentum_along_z_axis
+        initial_positions[:, 1:4], waist_radius, angular_momentum_along_z_axis
     )
 
     return 0

plot_beams.py
@@ -5,9 +5,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 from electrodynamics.beams import (
+    PolarizationVector,
     compute_electric_and_magnetic_field_for_laguerre_gauss_beam,
     compute_gaussian_beam_electric_field,
 )
+from electrodynamics.constants import lmbd
 from electrodynamics.typing import RealArray
 
 
@@ -118,7 +120,7 @@ def main() -> int:
     )
 
     parser.add_argument(
-        "--beam-type", default="gaussian", choices=["gaussian", "laguerre-gauss"]
+        "--beam-type", default="laguerre-gauss", choices=["gaussian", "laguerre-gauss"]
     )
 
     args = parser.parse_args()
@@ -157,11 +159,11 @@ def main() -> int:
     elif beam_type == "laguerre-gauss":
         print("Making plots for Laguerre-Gauss beam")
 
-        nx = 128
-        ny = 128
+        nx = 256
+        ny = 256
 
-        xs = np.linspace(-5, 5, nx)
-        ys = np.linspace(-5, 5, ny)
+        xs = np.linspace(-4e6, 4e6, nx)
+        ys = np.linspace(-4e6, 4e6, ny)
         X, Y = np.meshgrid(xs, ys, indexing="ij", sparse=False)
 
         positions = np.vstack((X.ravel(), Y.ravel())).T
@@ -169,19 +171,30 @@ def main() -> int:
             (positions, np.zeros((positions.shape[0], 1), dtype=np.float64)), axis=1
         )
 
+        wavelength = lmbd
+        waist_radius = 75 * wavelength
+        radial_index = 2
+        azimuthal_index = -2
+        polarization = PolarizationVector(1, 0)
         time = 0
-        azimuthal_index = 1
-        radial_index = 0
 
         electric_field, _magnetic_field = (
             compute_electric_and_magnetic_field_for_laguerre_gauss_beam(
-                positions, time, azimuthal_index, radial_index
+                amplitude=1,
+                waist_radius=waist_radius,
+                wavelength=wavelength,
+                radial_index=radial_index,
+                azimuthal_index=azimuthal_index,
+                polarization=polarization,
+                positions=positions,
+                time=time,
             )
         )
 
         plot_laguerre_gauss_beam_electric_field_cross_section(
             positions[:, 0].reshape(nx, ny),
             positions[:, 1].reshape(nx, ny),
+            # np.real(electric_field[:, 1]).reshape(nx, ny),
             np.linalg.vector_norm(electric_field, axis=-1).reshape(nx, ny),
             azimuthal_index,
             radial_index,

src/electrodynamics/beams.py
@@ -3,6 +3,8 @@ from dataclasses import dataclass
 import numpy as np
 import scipy as sp
 
+from electrodynamics.constants import c
+
 from .typing import ComplexArray, RealArray
 
 
@@ -13,7 +15,7 @@ class PolarizationVector:
 
     def __init__(self, x: complex, y: complex):
         if not np.isclose(abs(x) ** 2 + abs(y) ** 2, 1):
-            raise ValueError("|x|^2 + |y|^2 must be equal/close to 1")
+            raise ValueError("|x|^2 + |y|^2 must be approximately equal to 1")
 
         self.x = x
         self.y = y
@@ -68,6 +70,8 @@ def compute_electric_and_magnetic_field_for_plane_wave(
     k = 1.0
     omega = 1.0
 
+    # TODO: polarization
+
     E_x = E_0 * np.cos(k * z - omega * time)
     E_y = np.zeros_like(E_x)
     E_z = np.zeros_like(E_x)
@@ -89,7 +93,7 @@ def compute_electric_and_magnetic_field_for_plane_wave(
 
 
 def compute_electric_and_magnetic_field_for_gaussian_beam(
-    positions: RealArray, time: RealArray
+    polarization: PolarizationVector, positions: RealArray, time: RealArray
 ) -> tuple[RealArray, RealArray]:
     assert positions.shape[-1] == 3, "Positions must be 3D vectors"
 
@@ -103,9 +107,6 @@ def compute_electric_and_magnetic_field_for_gaussian_beam(
 
     # Frequency / angular speed
     omega = 1.0
-
-    # TODO: implement polarization support
-    polarization = 1.0
 
     wavelength = 1
 
@@ -139,9 +140,8 @@ def compute_electric_and_magnetic_field_for_gaussian_beam(
         )
     )
 
-    # TODO: handle polarization
-    E_x = amplitude * polarization * coefficients
-    E_y = np.zeros_like(E_x)
+    E_x = polarization.x * amplitude * coefficients
+    E_y = polarization.y * amplitude * coefficients
     # TODO: better approximation formula for computing derivatives in the paraxial approximation
     E_z = (2j) / (wave_number * w_z**2) * (x * E_x + y * E_y)
 
@@ -158,10 +158,14 @@ def compute_electric_and_magnetic_field_for_gaussian_beam(
 
 
 def compute_electric_and_magnetic_field_for_laguerre_gauss_beam(
+    amplitude: float,
+    waist_radius: float,
+    wavelength: float,
+    radial_index: int,
+    azimuthal_index: int,
+    polarization: PolarizationVector,
     positions: RealArray,
     time: float | RealArray,
-    l: int,
-    p: int,
 ) -> tuple[RealArray, RealArray]:
     assert positions.shape[-1] == 3, "Positions must be 3D vectors"
 
@@ -171,62 +175,54 @@ def compute_electric_and_magnetic_field_for_laguerre_gauss_beam(
     phi = np.arctan2(y, x)
     z = positions[:, 2]
 
-    # E_0
-    amplitude = 1.0
-
-    # Frequency / angular speed
-    omega = 1.0
-
-    # TODO: implement polarization support
-    polarization = 1.0
-
-    wavelength = 1
-
     # w_0 = w(0)
-    waist_radius = 2 * wavelength
-    refractive_index = 1.0
-    rayleigh_range = (np.pi * waist_radius**2 * refractive_index) / wavelength
-    w_z = waist_radius * np.sqrt(1 + (z / rayleigh_range) ** 2)
+    rayleigh_length = (np.pi * waist_radius**2) / wavelength
+    # FWHM
+    w_z = waist_radius * np.sqrt(1 + (z / rayleigh_length) ** 2)
+
+    omega = c * (2 * np.pi) / wavelength
 
     # k
-    wave_number = (2 * np.pi * refractive_index) / wavelength
+    wave_number = (2 * np.pi) / wavelength
     # R(z)
     zero_z = np.isclose(z, 0)
     curvature_radius = np.where(
-        zero_z, np.inf, z * (1 + np.square(rayleigh_range / np.where(zero_z, 1, z)))
+        zero_z, np.inf, z * (1 + np.square(rayleigh_length / np.where(zero_z, 1, z)))
     )
 
-    gouy_phase = np.arctan(z / rayleigh_range)
+    gouy_phase = np.arctan(z / rayleigh_length)
 
     r_over_wz_all_squared = (r / w_z) ** 2
 
     coefficients = (
-        (waist_radius / w_z)
-        * np.pow(np.sqrt(2) * r / w_z, abs(l))
+        sp.special.poch((radial_index + 1), abs(azimuthal_index))
+        * (waist_radius / w_z)
+        * np.pow(np.sqrt(2) * r / w_z, abs(azimuthal_index))
         # TODO: use a faster approximation of 1F1
-        * sp.special.hyp1f1(-p, abs(l) + 1, 2 * r_over_wz_all_squared)
+        * sp.special.hyp1f1(
+            -radial_index, abs(azimuthal_index) + 1, 2 * r_over_wz_all_squared
+        )
         * np.exp(-r_over_wz_all_squared)
         * np.exp(
             -1j
             * (
                 wave_number * z
                 + wave_number * (r**2) / (2 * curvature_radius)
-                + l * phi
-                - (2 * p + abs(l) + 1) * gouy_phase
+                + azimuthal_index * phi
+                - (2 * radial_index + abs(azimuthal_index) + 1) * gouy_phase
             )
             + 1j * omega * time
         )
     )
 
-    # TODO: handle polarization
-    E_x = amplitude * polarization * coefficients
-    E_y = np.zeros_like(E_x)
+    E_x = polarization.x * amplitude * coefficients
+    E_y = polarization.y * amplitude * coefficients
+
     # TODO: better approximation formula for computing derivatives in the paraxial approximation
     E_z = (2j) / (wave_number * w_z**2) * (x * E_x + y * E_y)
 
     E = np.real(np.stack((E_x, E_y, E_z), axis=-1)).astype(np.float64)
 
-    c = 1.0
     B_x = -E_y / c
     B_y = E_x / c
     B_z = 1j / (omega * w_z**2) * (y * E_x - x * E_y)

src/electrodynamics/plotting.py
@@ -1,3 +1,4 @@
+import numpy as np
 from matplotlib.figure import Figure
 import matplotlib.pyplot as plt
 
@@ -23,22 +24,40 @@ def plot_particle_positions(fig: Figure, positions: RealArray) -> None:
 
 
 def plot_angular_momentum_distribution(
-    initial_positions: RealArray, momenta: RealArray
+    initial_positions: RealArray, waist_radius: float, momenta: RealArray
 ) -> None:
+    print("Plotting angular momentum distribution of initial conditions")
+
     fig = plt.figure(dpi=200)
 
     ax = fig.add_subplot()
 
     ax.set_title("Normalized angular momentum distribution")
 
-    p = ax.scatter(initial_positions[:, 0], initial_positions[:, 1], c=momenta, s=3)
+    momenta /= momenta.max()
+
+    momenta = np.ma.masked_where(np.abs(momenta) < 1e-2, momenta)
+    initial_positions = np.ma.masked_array(
+        initial_positions, mask=np.repeat(momenta.mask.reshape(-1, 1), 3, axis=1)
+    )
+
+    momenta = np.ma.compress_nd(momenta, axis=0)
+    initial_positions = np.ma.compress_nd(initial_positions, axis=0)
+
+    p = ax.scatter(
+        initial_positions[:, 0] / waist_radius,
+        initial_positions[:, 1] / waist_radius,
+        c=momenta,
+        cmap="coolwarm",
+        s=3,
+    )
 
     # ax.pcolormesh(Z, R, np.real(electric_field), vmin=-1, vmax=1.0, cmap="coolwarm")
 
     fig.colorbar(p)
 
-    ax.set_xlabel("$x$")
-    ax.set_ylabel("$y$")
+    ax.set_xlabel("$x/w_0$")
+    ax.set_ylabel("$y/w_0$")
 
     ax.set_aspect(1)
 